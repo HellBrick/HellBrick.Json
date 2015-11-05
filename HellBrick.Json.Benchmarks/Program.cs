9b7807eeb7e9e870ad5563da1c811044d0ea5c5e
HellBrick <hellbrick@gmail.com>
2015-11-05 17:27:50 +0300

    Replaced Program.cs with a benchmark switch

--- HellBrick.Json.Benchmarks/Program.cs
+++ HellBrick.Json.Benchmarks/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using BenchmarkDotNet;
@@ -13,7 +14,13 @@ namespace HellBrick.Json.Benchmarks
 	{
 		static void Main( string[] args )
 		{
-			new BenchmarkRunner().RunCompetition( new FlatClassSerializationBenchmark() );
+			Type[] benchmarkTypes = typeof( Program ).Assembly.GetTypes()
+				.Where( t => t.GetTypeInfo().GetMethods().Any( m => m.GetCustomAttribute<BenchmarkAttribute>() != null ) )
+				.OrderBy( t => t.Namespace )
+				.ToArray();
+
+			BenchmarkCompetitionSwitch benchmarkSwitch = new BenchmarkCompetitionSwitch( benchmarkTypes );
+			benchmarkSwitch.Run( args );
 		}
 	}
 }
