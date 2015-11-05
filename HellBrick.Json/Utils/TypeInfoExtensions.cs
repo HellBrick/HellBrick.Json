1767453769b17d37d68bbc0ee00fba42fa455f81
HellBrick <hellbrick@gmail.com>
2015-11-05 10:38:24 +0300

    Extracted TypeInfo.IsNullable() extension

---
+++ HellBrick.Json/Utils/TypeInfoExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellBrick.Json.Utils
+{
+	public static class TypeInfoExtensions
+	{
+		public static bool IsNullable( this TypeInfo typeInfo ) => typeInfo.IsGenericType && typeInfo.GetGenericTypeDefinition() == typeof( Nullable<> );
+	}
+}
