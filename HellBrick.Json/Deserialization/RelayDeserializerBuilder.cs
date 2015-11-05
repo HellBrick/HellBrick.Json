210f3889f513ddcccd9d6ed3ea2e9a798243ff9a
HellBrick <hellbrick@gmail.com>
2015-11-06 00:07:46 +0300

    Added RelayDeserializerBuilder

---
+++ HellBrick.Json/Deserialization/RelayDeserializerBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Linq.Expressions.Expression;
+using static HellBrick.Json.Deserialization.ExpressionFactory;
+
+namespace HellBrick.Json.Deserialization
+{
+	internal abstract class RelayDeserializerBuilder<TOuter, TInner> : IDeserializerBuilder<TOuter>
+	{
+		public Expression BuildDeserializationExpression( Expression reader ) => ConvertToOuter( Deserialize( typeof( TInner ), reader ) );
+		protected abstract Expression ConvertToOuter( Expression inner );
+	}
+}
