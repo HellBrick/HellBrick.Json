3fc8a5a6e01b8c6599de2e0e84d789b13547a393
HellBrick <hellbrick@gmail.com>
2015-11-06 15:20:36 +0300

    Added tests for int, long, string, double, bool and DateTime

---
+++ HellBrick.Json.Test/Assertions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Numeric;
+using FluentAssertions.Primitives;
+
+namespace HellBrick.Json.Test
+{
+	internal static class Assertions
+	{
+		public static StringAssertions RoundTripThrough<T>( this StringAssertions target, T value )
+		{
+			target.DeserializeTo( value );
+			value.Should().SerializeTo<T>( target.Subject );
+			return target;
+		}
+
+		public static StringAssertions DeserializeTo<T>( this StringAssertions target, T value )
+		{
+			JsonFactory.DeserializerFor<T>().Deserialize( target.Subject ).Should().Be( value );
+			return target;
+		}
+
+		public static ObjectAssertions SerializeTo<T>( this ObjectAssertions target, string json )
+		{
+			JsonFactory.SerializerFor<T>().Serialize( (T) target.Subject ).Should().Be( json );
+			return target;
+		}
+	}
+}
---
+++ HellBrick.Json.Test/DateTimeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace HellBrick.Json.Test
+{
+	public class DateTimeTests
+	{
+		[Fact]
+		public void UtcDate()
+		{
+			DateTime date = new DateTime( 2015, 11, 06, 14, 25, 00, DateTimeKind.Utc );
+			const string json = "\"2015-11-06T14:25:00Z\"";
+			json.Should().RoundTripThrough( date );
+		}
+
+		[Fact]
+		public void LocalDate()
+		{
+			DateTime date = new DateTime( 2015, 11, 06, 14, 25, 00, DateTimeKind.Local );
+			string json = $"\"2015-11-06T14:25:00{date.ToString( "zzz" )}\"";
+			json.Should().RoundTripThrough( date );
+		}
+
+		[Fact]
+		public void UnspecifiedDate()
+		{
+			DateTime date = new DateTime( 2015, 11, 06, 14, 25, 00, DateTimeKind.Unspecified );
+			const string json = "\"2015-11-06T14:25:00\"";
+			json.Should().RoundTripThrough( date );
+		}
+	}
+}
