f060638a09df241bb33e09a672b5d181f8e16a12
HellBrick <hellbrick@gmail.com>
2015-11-12 02:59:14 +0300

    Added basic collection tests

---
+++ HellBrick.Json.Test/CollectionAssertions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace HellBrick.Json.Test
+{
+	public static class CollectionAssertions
+	{
+		public static string ShouldRoundTripThroughUnordered<TCollection, TItem>( this string json, TCollection collection ) where TCollection : IEnumerable<TItem>
+		{
+			json
+				.ShouldDeserializeToUnordered<TCollection, TItem>( collection )
+				.ShouldSerializeTo<TCollection, TItem>( json );
+
+			return json;
+      }
+
+		public static string ShouldRoundTripThroughOrdered<TCollection, TItem>( this string json, TCollection collection ) where TCollection : IEnumerable<TItem>
+		{
+			json
+				.ShouldDeserializeToOrdered<TCollection, TItem>( collection )
+				.ShouldSerializeTo<TCollection, TItem>( json );
+
+			return json;
+		}
+
+		public static string ShouldSerializeTo<TCollection, TItem>( this TCollection collection, string json ) where TCollection : IEnumerable<TItem>
+		{
+			JsonFactory.SerializerFor<TCollection>().Serialize( collection ).Should().Be( json );
+			return json;
+		}
+
+		public static TCollection ShouldDeserializeToUnordered<TCollection, TItem>( this string json, TCollection collection ) where TCollection : IEnumerable<TItem>
+		{
+			TCollection deserialized = JsonFactory.DeserializerFor<TCollection>().Deserialize( json );
+			if ( Object.ReferenceEquals( collection, null ) )
+				deserialized.Should().BeNull();
+			else
+				deserialized.Should().BeEquivalentTo( collection );
+
+			return collection;
+		}
+
+		public static TCollection ShouldDeserializeToOrdered<TCollection, TItem>( this string json, TCollection collection ) where TCollection : IEnumerable<TItem>
+		{
+			TCollection deserialized = JsonFactory.DeserializerFor<TCollection>().Deserialize( json );
+
+			if ( Object.ReferenceEquals( collection, null ) )
+			{
+				deserialized.Should().BeNull();
+			}
+			else
+			{
+				deserialized.Should()
+					.HaveCount( collection.Count() )
+					.And.ContainInOrder( collection );
+			}
+
+			return collection;
+		}
+	}
+}
---
+++ HellBrick.Json.Test/CollectionTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace HellBrick.Json.Test.CollectionTests
+{
+	public class ArrayTests
+	{
+		[Fact]
+		public void Empty()
+		{
+			int[] array = Array.Empty<int>();
+			string json = "[]";
+			json.ShouldRoundTripThroughOrdered<int[], int>( array );
+		}
+
+		[Fact]
+		public void NonEmpty()
+		{
+			int[] array = { 42, 256, 128 };
+			string json = "[42,256,128]";
+			json.ShouldRoundTripThroughOrdered<int[], int>( array );
+		}
+
+		[Fact]
+		public void Null()
+		{
+			int[] array = null;
+			string json = "null";
+			json.ShouldRoundTripThroughOrdered<int[], int>( array );
+		}
+	}
+
+	public class ListTests
+	{
+		[Fact]
+		public void Empty()
+		{
+			List<int> list = new List<int>();
+			string json = "[]";
+			json.ShouldRoundTripThroughOrdered<List<int>, int>( list );
+		}
+
+		[Fact]
+		public void NonEmpty()
+		{
+			List<int> list = new List<int>() { 42, 256, 128 };
+			string json = "[42,256,128]";
+			json.ShouldRoundTripThroughOrdered<List<int>, int>( list );
+		}
+
+		[Fact]
+		public void Null()
+		{
+			List<int> list = null;
+			string json = "null";
+			json.ShouldRoundTripThroughOrdered<List<int>, int>( list );
+		}
+	}
+
+	public class HashSetTests
+	{
+		[Fact]
+		public void Empty()
+		{
+			HashSet<int> set = new HashSet<int>();
+			string json = "[]";
+			json.ShouldRoundTripThroughUnordered<HashSet<int>, int>( set );
+		}
+
+		[Fact]
+		public void NonEmpty()
+		{
+			HashSet<int> set = new HashSet<int>() { 42, 256, 128 };
+			string json = "[42,256,128]";
+			json.ShouldRoundTripThroughUnordered<HashSet<int>, int>( set );
+		}
+
+		[Fact]
+		public void Null()
+		{
+			HashSet<int> set = null;
+			string json = "null";
+			json.ShouldRoundTripThroughUnordered<HashSet<int>, int>( set );
+		}
+
+		[Fact]
+		public void DuplicatesAreDiscardedOnDeserialization()
+		{
+			string json = "[42,256,128,42,42]";
+			HashSet<int> set = new HashSet<int>() { 42, 256, 128 };
+			json.ShouldDeserializeToUnordered<HashSet<int>, int>( set );
+		}
+	}
+}
