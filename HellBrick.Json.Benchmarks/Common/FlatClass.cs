4e0949715de5a051ba8d782ad9f2cd1cb8547dfe
HellBrick <hellbrick@gmail.com>
2015-11-28 13:47:17 +0300

    Added more properties to FlatClass

--- HellBrick.Json.Benchmarks/Common/FlatClass.cs
+++ HellBrick.Json.Benchmarks/Common/FlatClass.cs
@@ -8,10 +8,21 @@ namespace HellBrick.Json.Benchmarks.Common
 {
 	public class FlatClass
 	{
-		public static FlatClass Create() => new FlatClass() { Number = 42, Text = "64", TimeStamp = DateTime.UtcNow };
+		public static FlatClass Create() => new FlatClass()
+		{
+			Number = 42,
+			Text = "64",
+			TimeStamp = DateTime.UtcNow,
+			Url = new Uri( "http://bogus.host/1234" ),
+			Duration = TimeSpan.FromMinutes( 42 ),
+			Flag = true
+		};
 
 		public int Number { get; set; }
 		public string Text { get; set; }
 		public DateTime TimeStamp { get; set; }
+		public Uri Url { get; set; }
+		public TimeSpan? Duration { get; set; }
+		public bool Flag { get; set; }
 	}
 }
