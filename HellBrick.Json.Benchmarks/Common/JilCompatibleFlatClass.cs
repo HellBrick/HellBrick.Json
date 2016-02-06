5d52ea847d4fa3245f6e4f5004da765b23d243fc
HellBrick <hellbrick@gmail.com>
2016-02-06 13:34:56 +0300

    Added Jil-compatible benchmark

---
+++ HellBrick.Json.Benchmarks/Common/JilCompatibleFlatClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellBrick.Json.Benchmarks.Common
+{
+	public class JilCompatibleFlatClass
+	{
+		public static JilCompatibleFlatClass Create() => new JilCompatibleFlatClass()
+		{
+			Number = 42,
+			Text = "64",
+			TimeStamp = DateTime.UtcNow,
+			Duration = TimeSpan.FromMinutes( 42 ),
+			Flag = true
+		};
+
+		public int Number { get; set; }
+		public string Text { get; set; }
+		public DateTime TimeStamp { get; set; }
+		public TimeSpan? Duration { get; set; }
+		public bool Flag { get; set; }
+	}
+}
